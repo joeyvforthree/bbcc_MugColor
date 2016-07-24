88cb96b7aac758398b814e1974816a721d27150a
Joe Velardo <j.velardo24@gmail.com>
2016-07-23 21:52:44 -0400

    script complete

---
+++ mug_color.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+#Problem        : Mug Color
+#Language       : Python
+#Version        : Python 2.7.8
+#by Joe Velardo
+#Input for your program will be provided from STDIN
+#Print out all output from your program to STDOUT
+
+from __future__ import print_function
+import sys
+
+colors = ["White", "Black", "Blue", "Red", "Yellow"]
+not_these = []
+count = 0
+
+num = raw_input()
+num = int(num)
+
+while count < num:
+      color_entry = raw_input()
+      not_these.append(str(color_entry))
+      count += 1
+
+for x in not_these:
+    for y in colors:
+        if x == y:
+           colors.remove(y)
+
+print("The mug is "+ colors[0])
